1d7ebe53e34306a7a30b064f945ce2a665909734
LucasLM1 <lucascorreia325@gmail.com>
2021-08-05 16:49:04 -0300

    Atualizando Blog Pessoal

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/UserDetailsImplementations.java
@@ -0,0 +1,66 @@
+package org.generation.blogPessoal.seguranca;
+
+import java.util.Collection;
+
+import org.generation.blogPessoal.model.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImplementations implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String userName;
+	private String password;
+	
+	public UserDetailsImplementations(Usuario user) {
+		this.userName = user.getUsuario();
+		this.userName = user.getSenha();
+	}
+	
+	public UserDetailsImplementations() {}
+	
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return userName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
